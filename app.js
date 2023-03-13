8067ed824adf82648972d7182d4289a7537693f1
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-03-13 18:24:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const age = prompt("Yoshingizni Kiriting:")
+
+// if (age <= 18) {
+//     alert("Siz Hali Voyaga Yetmagansiz")
+// } else {
+//     alert("Sizga qanday yordam kerak")
+// }
+
+// age < 18 ? alert("Siz Hali Voyaga Yetmagansiz") : alert("Sizga qanday yordam kerak")
+
+//  Armiya yoshi 18 / 28
+age < 18 ? alert(`Siz Armiya yoshidan ${18 - age} kichiksiz ozgina chidang yoki Russiayaga keting 😆`) : age < 28 ? alert(`Siz Armiya borishingiz mumkin `) : alert(`Siz armiya yoshidan  ${age - 27} yosh o'tib bo'lgansiz`)
